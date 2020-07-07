95c96bc3d4340b3ec0704b847ef57c34ce2e036d
unknown <brittneybee82@gmail.com>
2020-07-07 19:02:11 +0200

    Connected home page and default page , changed line height for footer

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,15 @@
 import React, { Fragment } from 'react';
 import './Home.css';
-import image from './image.jpeg';
 
 const Home = () => {
   return (
     <Fragment>
       <div className="homeContainer">
-        <img className="home-image" src={image} alt="img" />
+        <img
+          classname="home-img"
+          src="https://i.postimg.cc/hGyC5P8Z/Whats-App-Image-2020-06-26-at-3-02-46-PM.jpg"
+          alt=""
+        />
         <div className="homeContent">
           <h1 className="home-heading">Hi . I'm Brittney Bowers</h1>
           <p className="home-text">A junior frontend developer</p>

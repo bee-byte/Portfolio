cd9a3f5030b07524fc86fbc688f14b5e263926d0
unknown <brittneybee82@gmail.com>
2020-07-07 16:45:19 +0200

    created a progress chart because the progress bar crashes the system

--- src/components/pages/ProgressBar.js
+++ src/components/pages/ProgressBar.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-const ProgressBar = (props) => {
-  const Filler = (props) => {
-    return <div className="filler" style={{ width: `${props.percentage}%` }} />;
-  };
+const ProgressBar = () => {
   return (
-    <ProgressBar>
-      <div className="progress-bar">
-        <Filler percentage={props.percentage} />
+    <Fragment className="LanguageHeading">
+      <div className="language-heading">
+        <div>
+          <h3>PYTHON = 70%</h3>
+          <img src="https://i.postimg.cc/DfW0Sj8t/python.png" alt="" />
+        </div>
+        <br />
+        <div>
+          <h3>JAVASCRIPT = 50%</h3>
+          <img src="https://i.postimg.cc/LXgS8WND/JS.png" alt="" />
+        </div>
+        <br />
+        <div>
+          <h3>REACT = 50%</h3>
+          <img src="https://i.postimg.cc/x184Qzsy/REACT.png" alt="" />
+        </div>
+        <br />
+        <div>
+          <h3>HTML = 75%</h3>
+          <img src="https://i.postimg.cc/d1wRsMsP/html.png" alt="" />
+        </div>
+        <br />
+        <div>
+          <h3>CSS 75%</h3>
+          <img src="https://i.postimg.cc/3rLjTbzV/CSS.png" alt="" />
+        </div>
       </div>
-    </ProgressBar>
+    </Fragment>
   );
 };
-
 export default ProgressBar;
